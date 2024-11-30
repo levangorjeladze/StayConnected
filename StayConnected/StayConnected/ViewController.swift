6d3cba7249a7c792b68ce21b7eda246dfa205b47
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-11-30 10:50:28 +0400

    Project created

---
+++ StayConnected/StayConnected/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StayConnected
+//
+//  Created by Levan Gorjeladze on 30.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
