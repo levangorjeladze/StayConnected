8d552eadf7c0ed83332044a63fcc409a0e17afba
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-11-30 14:33:45 +0400

    Sign up / Log in
    Sign up / Log in

--- StayConnected/StayConnected/Screens Folder/LoginScreen/LoginViewController.swift
+++ StayConnected/StayConnected/Screens Folder/LoginScreen/LoginViewController.swift
@@ -5,4 +5,155 @@
 //  Created by Levan Gorjeladze on 30.11.24.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Log in"
+        label.font = UIFont(name: "Inter", size: 30)
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.textAlignment = .left
+        label.alpha = 1
+        return label
+    }()
+    
+    let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.font = UIFont(name: "Inter", size: 12)
+        label.alpha = 1
+        return label
+    }()
+    
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter your email"
+        textField.borderStyle = .roundedRect
+        textField.alpha = 1
+        textField.font = UIFont(name: "Inter", size: 16)
+        return textField
+    }()
+    
+    
+    let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password"
+        label.font = UIFont(name: "Inter", size: 12)
+        label.alpha = 1
+        return label
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter your password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .roundedRect
+        textField.alpha = 1
+        textField.font = UIFont(name: "Inter", size: 16)
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log in", for: .normal)
+        button.alpha = 1
+        button.backgroundColor = UIColor(hex: "#4E53A2")
+        button.layer.cornerRadius = 12
+        button.setTitleColor(.white, for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets(top: 20, left: 24, bottom: 20, right: 24)
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.alpha = 1
+        button.setTitleColor(UIColor(hex: "#5E6366"), for: .normal)
+        button.titleLabel?.font = UIFont(name: "Anek Devanagari", size: 14)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(loginLabel)
+        view.addSubview(emailLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordLabel)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(signUpButton)
+        
+        setupConstraints()
+
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
+
+    @objc func signUpButtonTapped() {
+        let signUpViewController = SignUpViewController()
+        navigationController?.pushViewController(signUpViewController, animated: true)
+    }
+    
+    func setupConstraints() {
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 141),
+            loginLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 187),
+            loginLabel.widthAnchor.constraint(equalToConstant: 111),
+            loginLabel.heightAnchor.constraint(equalToConstant: 30),
+
+            emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            emailLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 30),
+
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 5),
+            emailTextField.widthAnchor.constraint(equalToConstant: 327),
+            emailTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 30),
+
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 5),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 327),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25.01),
+            loginButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 50),
+            loginButton.widthAnchor.constraint(equalToConstant: 342.7),
+            loginButton.heightAnchor.constraint(equalToConstant: 59),
+            
+            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 10),
+            signUpButton.widthAnchor.constraint(equalToConstant: 60),
+            signUpButton.heightAnchor.constraint(equalToConstant: 21)
+        ])
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized = String(hexSanitized.dropFirst())
+        }
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
--- StayConnected/StayConnected/Screens Folder/SignUpScreen/SignUpViewController.swift
+++ StayConnected/StayConnected/Screens Folder/SignUpScreen/SignUpViewController.swift
@@ -5,4 +5,197 @@
 //  Created by Levan Gorjeladze on 30.11.24.
 //
 
-import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    let signUpLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign Up"
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .black
+        label.textAlignment = .left
+        label.alpha = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Full Name"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .black
+        label.alpha = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let fullNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter your full name"
+        textField.borderStyle = .none
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 8
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.alpha = 1
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .black
+        label.alpha = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter your email"
+        textField.borderStyle = .none
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 8
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.alpha = 1
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .black
+        label.alpha = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter your password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .none
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 8
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.alpha = 1
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let confirmPasswordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Confirm Password"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .black
+        label.alpha = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    let confirmPasswordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Confirm your password"
+        textField.isSecureTextEntry = true
+        textField.borderStyle = .none
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.cornerRadius = 8
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.alpha = 1
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.alpha = 1
+        button.backgroundColor = UIColor(hex: "#4E53A2")
+        button.layer.cornerRadius = 12
+        button.setTitleColor(.white, for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets(top: 20, left: 24, bottom: 20, right: 24)
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(signUpLabel)
+        view.addSubview(fullNameLabel)
+        view.addSubview(fullNameTextField)
+        view.addSubview(usernameLabel)
+        view.addSubview(usernameTextField)
+        view.addSubview(passwordLabel)
+        view.addSubview(passwordTextField)
+        view.addSubview(confirmPasswordLabel)
+        view.addSubview(confirmPasswordTextField)
+        view.addSubview(signUpButton)
+        
+        setupConstraints()
+
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            signUpLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 18),
+            signUpLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 107),
+            signUpLabel.widthAnchor.constraint(equalToConstant: 120),
+            signUpLabel.heightAnchor.constraint(equalToConstant: 30),
+
+            fullNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            fullNameLabel.topAnchor.constraint(equalTo: signUpLabel.bottomAnchor, constant: 50),
+            fullNameLabel.widthAnchor.constraint(equalToConstant: 327),
+            fullNameLabel.heightAnchor.constraint(equalToConstant: 15),
+
+            fullNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            fullNameTextField.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 10),
+            fullNameTextField.widthAnchor.constraint(equalToConstant: 327),
+            fullNameTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            usernameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            usernameLabel.topAnchor.constraint(equalTo: fullNameTextField.bottomAnchor, constant: 30),
+
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            usernameTextField.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 5),
+            usernameTextField.widthAnchor.constraint(equalToConstant: 327),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            passwordLabel.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 30),
+
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 5),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 327),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            confirmPasswordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            confirmPasswordLabel.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
+
+            confirmPasswordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            confirmPasswordTextField.topAnchor.constraint(equalTo: confirmPasswordLabel.bottomAnchor, constant: 5),
+            confirmPasswordTextField.widthAnchor.constraint(equalToConstant: 327),
+            confirmPasswordTextField.heightAnchor.constraint(equalToConstant: 52),
+
+            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25.01),
+            signUpButton.topAnchor.constraint(equalTo: confirmPasswordTextField.bottomAnchor, constant: 50),
+            signUpButton.widthAnchor.constraint(equalToConstant: 342.7),
+            signUpButton.heightAnchor.constraint(equalToConstant: 59)
+        ])
+    }
+
+    @objc func signUpButtonTapped() {
+        print("Sign up button tapped")
+    }
+}
