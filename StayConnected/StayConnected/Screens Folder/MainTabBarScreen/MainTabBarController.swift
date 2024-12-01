07a1c180f72dd268b92bebaf4ba3537710f50bad
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-12-01 21:24:35 +0400

    fix
    fix tab bar and small changes leaderboar/profile screens

--- StayConnected/StayConnected/Screens Folder/MainTabBarScreen/MainTabBarController.swift
+++ StayConnected/StayConnected/Screens Folder/MainTabBarScreen/MainTabBarController.swift
@@ -25,21 +25,19 @@ class MainTabBarController: UITabBarController {
     }
     
     private func setupTabBar() {
-        
         let homeScreen = HomeViewController()
         guard let homeImage = UIImage(systemName: "house"), let homeFilledImage = UIImage(systemName: "house.fill") else { return }
         homeScreen.tabBarItem = UITabBarItem(title: "Home", image: homeImage, selectedImage: homeFilledImage)
         
-        let leaderBoardScreen = UIViewController()
-        guard let leaderBoardImage = UIImage(systemName: "trophy"), let leaderBoarFilled = UIImage(systemName: "trophy.fill") else { return }
-        leaderBoardScreen.tabBarItem = UITabBarItem(title: "LeaderBoard", image: leaderBoardImage, selectedImage: leaderBoarFilled)
+        let leaderBoardScreen = LeaderboardViewController() // გამოიყენეთ LeaderboardViewController
+        guard let leaderBoardImage = UIImage(systemName: "trophy"), let leaderBoardFilled = UIImage(systemName: "trophy.fill") else { return }
+        leaderBoardScreen.tabBarItem = UITabBarItem(title: "LeaderBoard", image: leaderBoardImage, selectedImage: leaderBoardFilled)
         
-        let profileScreen = UIViewController()
-        guard let personImage = UIImage(systemName: "person"), let personFilledImage = UIImage(systemName: "person.fill") else {return }
+        let profileScreen = ProfileViewController() // გამოიყენეთ ProfileViewController
+        guard let personImage = UIImage(systemName: "person"), let personFilledImage = UIImage(systemName: "person.fill") else { return }
         profileScreen.tabBarItem = UITabBarItem(title: "Profile", image: personImage, selectedImage: personFilledImage)
         
         let controllers = [homeScreen, leaderBoardScreen, profileScreen]
-        
         self.viewControllers = controllers.map { UINavigationController(rootViewController: $0) }
         tabBar.backgroundColor = .systemGray5
         tabBar.tintColor = UIColor(hex: "#4E53A2")
