d85cf65d4d09c918fd9bcaab87a8052b1b24d00f
KobaSinauridze <Sinauridze.Koba@tbcacademy.edu.ge>
2024-12-02 23:31:47 +0400

    AddQuestionScreen UI

--- StayConnected/StayConnected/Screens Folder/MainTabBarScreen/MainTabBarController.swift
+++ StayConnected/StayConnected/Screens Folder/MainTabBarScreen/MainTabBarController.swift
@@ -30,16 +30,16 @@ class MainTabBarController: UITabBarController {
         guard let homeImage = UIImage(systemName: "house"), let homeFilledImage = UIImage(systemName: "house.fill") else { return }
         homeScreen.tabBarItem = UITabBarItem(title: "Home", image: homeImage, selectedImage: homeFilledImage)
         
-        let leaderBoardScreen = UIViewController()
-        guard let leaderBoardImage = UIImage(systemName: "trophy"), let leaderBoarFilled = UIImage(systemName: "trophy.fill") else { return }
-        leaderBoardScreen.tabBarItem = UITabBarItem(title: "LeaderBoard", image: leaderBoardImage, selectedImage: leaderBoarFilled)
-        
-        let profileScreen = UIViewController()
-        guard let personImage = UIImage(systemName: "person"), let personFilledImage = UIImage(systemName: "person.fill") else {return }
+        let leaderBoardScreen = LeaderboardViewController() // Leaderboard ეკრანის ინიციალიზაცია და მისი ტაბ-ბარის შექმნა
+        guard let leaderBoardImage = UIImage(systemName: "trophy"), let leaderBoardFilled = UIImage(systemName: "trophy.fill") else { return }
+        leaderBoardScreen.tabBarItem = UITabBarItem(title: "LeaderBoard", image: leaderBoardImage, selectedImage: leaderBoardFilled)
+
+        let profileScreen = ProfileViewController() // Profile ეკრანის ინიციალიზაცია და მისი ტაბბარის შექმნა
+        guard let personImage = UIImage(systemName: "person"), let personFilledImage = UIImage(systemName: "person.fill") else { return }
         profileScreen.tabBarItem = UITabBarItem(title: "Profile", image: personImage, selectedImage: personFilledImage)
-        
+
+    
         let controllers = [homeScreen, leaderBoardScreen, profileScreen]
-        
         self.viewControllers = controllers.map { UINavigationController(rootViewController: $0) }
         tabBar.backgroundColor = .systemGray5
         tabBar.tintColor = UIColor(hex: "#4E53A2")
